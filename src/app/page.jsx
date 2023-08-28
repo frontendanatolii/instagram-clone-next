10e12e85dddfa364cbc1d73c0fe4822c2e346d78
Tolik Petras <toliketras4@gmail.com>
2023-08-28 13:25:07 +0300

    completed

---
+++ src/app/page.jsx
@@ -0,0 +1,23 @@
+import Feed from '@/components/Feed'
+import Header from '@/components/Header'
+import UploadModal from '@/components/UploadModal'
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      {/* Header */}
+
+      <Header />
+
+      {/* Feed */}
+
+      <Feed />
+
+      {/* Modal */}
+
+      <UploadModal />
+    </div>
+  )
+}
